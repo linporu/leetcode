29dbbc859cbc5aca46f5b573070990c25c1ddf22
linporu <qazwsx123456guy@gmail.com>
2025-01-12 02:41:11 +0800

    update 0141

--- 0141_linked_list_cycle.py
+++ 0141_linked_list_cycle.py
@@ -7,8 +7,8 @@ class ListNode:
         self.val = x
         self.next = None
 
-
-class Solution:
+# 標記法
+class Solution01:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
 
         if head is None:
@@ -23,3 +23,21 @@ class Solution:
 
             head.val = "passed"
             head = head.next
+
+
+# 快慢指針法
+class Solution02:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+
+        slow = head
+        fast = head.next
+
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next        # 移動一步
+            fast = fast.next.next   # 移動兩步
+
+        return True
