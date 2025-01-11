59b491eaa1f9fb3fd71c030c64a235c58d0749be
linporu <qazwsx123456guy@gmail.com>
2025-01-12 01:59:08 +0800

    update 0013

--- 0013_roman_to_integer.py
+++ 0013_roman_to_integer.py
@@ -1,21 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        symbol = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        value = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
-        list = [char for char in s]
+        sum = value[s[-1]]  # 先加入最後一個數字
 
-        value = 0
-
-        for i in range(len(list) - 1):
-            if list[i] == "I" and (list[i + 1] == "V" or list[i + 1] == "X"):
-                value -= 1
-            elif list[i] == "X" and (list[i + 1] == "L" or list[i + 1] == "C"):
-                value -= 10
-            elif list[i] == "C" and (list[i + 1] == "D" or list[i + 1] == "M"):
-                value -= 100
+        for i in range(len(s) - 2, -1, -1):  # 從倒數第二個開始往前
+            curr = value[s[i]]
+            prev = value[s[i + 1]]
+            if curr >= prev:
+                sum += curr
             else:
-                value += symbol[list[i]]
+                sum -= curr
 
-        value += symbol[list[len(list) - 1]]
-
-        return value
+        return sum
