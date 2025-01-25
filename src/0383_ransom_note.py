f7aaa4b2abb2d028e4e0cda01070c2e4a90fe0f2
linporu <qazwsx123456guy@gmail.com>
2025-01-25 22:27:59 +0800

    style: update 0383

--- src/0383_ransom_note.py
+++ src/0383_ransom_note.py
@@ -1,7 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        dict_magazine = {f"{c}":0 for c in "abcdefghijklmnopqrstuvwxyz"}
-        dict_ransomNote = {f"{c}":0 for c in "abcdefghijklmnopqrstuvwxyz"}
+        dict_magazine = {f"{c}": 0 for c in "abcdefghijklmnopqrstuvwxyz"}
+        dict_ransomNote = {f"{c}": 0 for c in "abcdefghijklmnopqrstuvwxyz"}
 
         for c in magazine:
             dict_magazine[f"{c}"] += 1
