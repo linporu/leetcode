d53b7f19501ef250ac1b741fcc0508ebaf626ce5
linporu <qazwsx123456guy@gmail.com>
2025-02-20 02:43:53 +0800

    style(0412): update import

--- src/0412_fizz_buzz.py
+++ src/0412_fizz_buzz.py
@@ -1,9 +1,12 @@
+from typing import List
+
+
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
 
         answer = []
 
-        for i in range(1, n +1):
+        for i in range(1, n + 1):
 
             if i % 3 == 0 and i % 5 == 0:
                 answer.append("FizzBuzz")
@@ -13,5 +16,5 @@ class Solution:
                 answer.append("Buzz")
             else:
                 answer.append(f"{i}")
-        
+
         return answer
