b971747c679085f4098e0d3f3f98f9f06f564623
linporu <qazwsx123456guy@gmail.com>
2025-02-19 22:31:14 +0800

    update 0101

--- src/0101_symmetric_tree.py
+++ src/0101_symmetric_tree.py
@@ -9,7 +9,8 @@ class TreeNode:
         self.right = right
 
 
-class Solution:
+# Recursive method
+class Solution01:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
 
         def check(node_left, node_right):
@@ -25,3 +26,29 @@ class Solution:
                 )
 
         return check(root.left, root.right)
+
+
+# Iterative method
+class Solution02:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        # Base cases
+        if not root.left and not root.right:
+            return True
+        if not root.left or not root.right:
+            return False
+
+        stack = [(root.left, root.right)]
+
+        while stack:
+            node_left, node_right = stack.pop()
+            if not node_left and not node_right:
+                continue
+            if not node_left or not node_right:
+                return False
+            if node_left.val != node_right.val:
+                return False
+
+            stack.append([node_left.left, node_right.right])
+            stack.append([node_left.right, node_right.left])
+
+        return True
