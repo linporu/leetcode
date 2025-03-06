a1da756126a65585ecb840da0bbfb11a333bbc0c
linporu <qazwsx123456guy@gmail.com>
2025-03-07 02:20:04 +0800

    upload 0589

---
+++ src/0589_n-ary_tree_preorder_traversal.py
@@ -0,0 +1,24 @@
+from typing import List, Optional
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        def dfs(node):
+            if not node:
+                return
+
+            result.append(node.val)
+
+            for child in node.children:
+                dfs(child)
+
+        result = []
+        dfs(root)
+        return result
