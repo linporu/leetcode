c4acd58146a1bdc1904b93d8c6dc67e8bf9f873e
linporu <qazwsx123456guy@gmail.com>
2025-03-08 01:01:46 +0800

    update 0590

--- src/0590_n-ary_tree_postorder_traversal.py
+++ src/0590_n-ary_tree_postorder_traversal.py
@@ -9,7 +9,7 @@ class Node:
 
 
 # Recursive method
-class Solution:
+class Solution01:
     def postorder(self, root: 'Node') -> List[int]:
         def dfs(node):
             if not node:
@@ -22,4 +22,29 @@ class Solution:
 
         result = []
         dfs(root)
+        return result
+
+
+# Iterative method
+class Solution02:
+    def postorder(self, root: 'Node') -> List[int]:
+        # Base case
+        if not root:
+            return []
+
+        result = []
+        stack = [(root, False)]
+
+        while stack:
+            node, is_visited = stack.pop()
+
+            if not is_visited:
+                stack.append((node, True))
+
+                for i in range(len(node.children) - 1, -1, -1):
+                    stack.append((node.children[i], False))
+
+            else:
+                result.append(node.val)
+
         return result
