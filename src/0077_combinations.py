720ce674fe77b5a8bacf995f1df425fac47df782
linporu <qazwsx123456guy@gmail.com>
2025-03-10 00:09:41 +0800

    update 0077

--- src/0077_combinations.py
+++ src/0077_combinations.py
@@ -1,7 +1,8 @@
 from typing import List
 
 
-class Solution:
+# Recursive approach
+class Solution01:
     def combine(self, n: int, k: int) -> List[List[int]]:
         def backtrack(choices, path):
             if len(path) == k:  # 當路徑長度達到 k 時收集結果
@@ -11,12 +12,36 @@ class Solution:
             remaining = set(choices)
             for choice in remaining:
                 path.append(choice)
-                new_choices = {x for x in remaining if x > choice}  # 只選擇比當前數字大的
+                new_choices = {
+                    new_choice for new_choice in remaining if new_choice > choice
+                }  # 只選擇比當前數字大的
                 backtrack(new_choices, path)
                 path.pop()
 
         result = []
         choices = {i for i in range(1, n + 1)}  # 修正範圍到 n
         backtrack(choices, [])
+
+        return result
+
+
+# Iterative approach
+class Solution02:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+        path = []
+        choices = {i for i in range(1, n + 1)}
+        stack = [(path, choices)]
+
+        while stack:
+            path, remaining = stack.pop()
+
+            if len(path) == k:
+                result.append(path.copy())
+
+            for choice in remaining:
+                new_path = path + [choice]
+                new_choices = {new_choice for new_choice in remaining if new_choice > choice}
+                stack.append((new_path, new_choices))
 
         return result
