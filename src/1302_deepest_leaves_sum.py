17151a8e277c9cf84420590a16aadd0aaf83cb86
linporu <qazwsx123456guy@gmail.com>
2025-03-09 21:00:17 +0800

    update 1302

--- src/1302_deepest_leaves_sum.py
+++ src/1302_deepest_leaves_sum.py
@@ -10,7 +10,7 @@ class TreeNode:
 
 
 # Iterative BFS
-class Solution:
+class Solution01:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
         from collections import deque
 
@@ -52,4 +52,36 @@ class Solution:
                 if node.right:
                     queue.append(node.right)
 
+        return deepest_leaves_sum
+
+
+# Iterative BFS refactored
+class Solution02:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        """
+        利用 BFS 特性，實現一次遍歷即得結果。
+        每層累加該層 node.val，進入下一層時歸零。
+        """
+        from collections import deque
+
+        # Base case
+        if not root:
+            return 0
+
+        queue = deque([root])
+        curr_depth = 0
+
+        while queue:
+            curr_depth += 1
+            level_size = len(queue)
+            deepest_leaves_sum = 0
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                deepest_leaves_sum += node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
         return deepest_leaves_sum
