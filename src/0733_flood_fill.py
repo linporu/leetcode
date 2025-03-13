a113d953c419dd57ea6877637575d3b2318ed233
linporu <qazwsx123456guy@gmail.com>
2025-03-13 12:24:29 +0800

    update 0733

--- src/0733_flood_fill.py
+++ src/0733_flood_fill.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 # Recursive DFS
-class Solution:
+class Solution01:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         def flood(r, c, starting_color, visited):
             if (r, c) in visited:
@@ -26,5 +26,38 @@ class Solution:
         starting_color = image[sr][sc]
         visited = set()
         flood(sr, sc, starting_color, visited)
+
+        return image
+
+
+# Iterative DFS
+class Solution02:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if not image:
+            return [[]]
+
+        starting_color = image[sr][sc]
+        visited = set()
+        stack = [(sr, sc)]
+
+        while stack:
+            r, c = stack.pop()
+
+            if r < 0 or r >= len(image):
+                continue
+            if c < 0 or c >= len(image[0]):
+                continue
+            if (r, c) in visited:
+                continue
+            if image[r][c] != starting_color:
+                continue
+
+            image[r][c] = color
+            visited.add((r, c))
+
+            stack.append((r + 1, c))
+            stack.append((r - 1, c))
+            stack.append((r, c + 1))
+            stack.append((r, c - 1))
 
         return image
