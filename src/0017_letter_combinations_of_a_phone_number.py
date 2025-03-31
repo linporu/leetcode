35805b5ca5366e7d8fa4951a1a9641ac94452a54
linporu <qazwsx123456guy@gmail.com>
2025-04-01 01:44:13 +0800

    upload 0017

---
+++ src/0017_letter_combinations_of_a_phone_number.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+# Recursive DFS
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        map = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        }
+
+        def backtrack(idx, path):
+            if idx == len(digits):
+                result.append(path)
+                return
+
+            curr_digit = digits[idx]
+
+            for letter in map[curr_digit]:
+                # 直接創建新字串並傳遞給下一層遞迴
+                backtrack(idx + 1, path + letter)
+
+        if not digits:
+            return []
+
+        result = []
+        backtrack(0, "")
+        return result
