dceca1e118180a037d24fdab8a9afda7cd19652d
linporu <qazwsx123456guy@gmail.com>
2025-04-08 17:21:05 +0800

    update 0455

--- src/0455_assign_cookies.py
+++ src/0455_assign_cookies.py
@@ -24,3 +24,25 @@ class Solution01:
                     curr_s += 1
 
         return count
+
+
+# Optimized two pointer approach
+class Solution02:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        num_children = len(g)
+        num_cookies = len(s)
+        count = 0
+        idx_child = 0
+        idx_cookie = 0
+
+        while idx_cookie < num_cookies and idx_child < num_children:
+            if g[idx_child] <= s[idx_cookie]:
+                idx_child += 1
+                idx_cookie += 1
+                count += 1
+            else:
+                idx_cookie += 1
+
+        return count
