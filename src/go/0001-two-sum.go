91dabe9a94f230da58755435663d0956a6e2decc
linporu <qazwsx123456guy@gmail.com>
2025-10-23 14:02:08 +0800

    feat(src/go): add solution for two-sum problem - implement twoSum function to find two indices in array that sum to target - use map to store and lookup complements efficiently

---
+++ src/go/0001-two-sum.go
@@ -0,0 +1,14 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	dict := map[int]int{}
+
+	for i := range nums {
+		complement := target - nums[i]
+		if value, exists := dict[complement]; exists {
+			return []int{i, value}
+		}
+		dict[nums[i]] = i
+	}
+	return []int{}
+}
