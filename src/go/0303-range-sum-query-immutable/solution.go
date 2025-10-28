fd6254fa44b152609bc333e0b9c36c75a2a43a88
linporu <qazwsx123456guy@gmail.com>
2025-10-28 15:51:22 +0800

    feat(leetcode0303): add initial solution for range sum query immutable - implement NumArray struct to store input array - add Constructor to initialize NumArray - add SumRange method to calculate sum from left to right index inclusively using a loop

---
+++ src/go/0303-range-sum-query-immutable/solution.go
@@ -0,0 +1,25 @@
+package leetcode0303
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(left,right);
+ */
+
+type NumArray struct {
+	nums []int
+}
+
+func Constructor(nums []int) NumArray {
+	return NumArray{
+		nums: nums,
+	}
+}
+
+func (this *NumArray) SumRange(left int, right int) int {
+	var sum int
+	for i := left; i <= right; i++ {
+		sum += this.nums[i]
+	}
+	return sum
+}
