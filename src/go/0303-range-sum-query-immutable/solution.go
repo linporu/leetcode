64dbf7aaa8c40040e729501af747eb697bc8f0c3
linporu <qazwsx123456guy@gmail.com>
2025-10-28 16:36:01 +0800

    feat(0303-range-sum-query-immutable): add prefix sum solution for range sum query - add NumArray2 struct implementing prefix sum approach - implement Constructor2 to build prefix sums array - implement SumRange method using prefix sums for O(1) query time - keep original NumArray and SumRange as loop-based solution

--- src/go/0303-range-sum-query-immutable/solution.go
+++ src/go/0303-range-sum-query-immutable/solution.go
@@ -6,6 +6,7 @@ package leetcode0303
  * param_1 := obj.SumRange(left,right);
  */
 
+// Solution 1: for loop
 type NumArray struct {
 	nums []int
 }
@@ -22,4 +23,42 @@ func (this *NumArray) SumRange(left int, right int) int {
 		sum += this.nums[i]
 	}
 	return sum
+}
+
+// Solution 2: prefix sum
+type NumArray2 struct {
+	nums []int
+	sums []int
+}
+
+func Constructor2(nums []int) NumArray2 {
+	if nums == nil {
+		return NumArray2{
+			nums: nil,
+			sums: nil,
+		}
+	}
+
+	sums := make([]int, len(nums))
+	sums[0] = nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		sums[i] = sums[i-1] + nums[i]
+	}
+
+	return NumArray2{
+		nums: nums,
+		sums: sums,
+	}
+}
+
+func (this *NumArray2) SumRange(left int, right int) int {
+	if this.sums == nil {
+		return 0
+	}
+
+	if left == 0 {
+		return this.sums[right]
+	}
+	return this.sums[right] - this.sums[left-1]
 }
